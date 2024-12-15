f5b65b93e9db5e300c2cab43bb772c176fe6ae5d
Snehit Sah <snehitsah@protonmail.com>
2024-12-15 15:30:14 +0530

    create build dir
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+import argparse
+import os
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                    prog='Linky',
+                    description='Generate static site of short links',
+                    epilog='created by Snehit Sah')
+    parser.add_argument('links')
+    parser.add_argument('-o', '--output-dir', default="build")
+
+    args = parser.parse_args()
+    print(args.links, args.output_dir)
+
+    try:
+        os.makedirs(args.output_dir, exist_ok=True)
+    except:
+        print("Couldn't create build directory at " + args.output_dir)
+
+
     print("hello world")
+    
